29207b523fe0286fec02746b8f2f8d7e0d1f0d5a
Abdulatif <abdulatifsh90@gmail.com>
2025-07-20 21:00:18 +0900

    feat: practice useState & useEffect hooks

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Route, Routes, useLocation } from 'react-router-dom';
-import HomePage  from './screens/homePage';
+// import HomePage  from './screens/homePage';
 import ProductsPage  from './screens/productsPage';
 import OrderPage  from './screens/orderPage';
 import  UserPage  from './screens/userPage';
 import  HomeNavbar  from './components/headers/HomeNavbar';
 import OtherNavbar  from './components/headers/OtherNavbar';
 import Footer from './components/footer';
+// import Test from './screens/Test';
 // import "../css/app.css
 import '../css/navbar.css';
 import "../css/footer.css"
@@ -22,7 +23,7 @@ function App() {
         <Route path="/products" element={<ProductsPage />} />
         <Route path="/orders" element={<OrderPage />} />
         <Route path="/member-page" element={<UserPage />} />
-        <Route path="/" element={<HomePage />} />
+        {/* <Route path="/" element={<Test />} /> */}
       </Routes>
       <Footer />
     </>
--- src/app/components/headers/HomeNavbar.tsx
+++ src/app/components/headers/HomeNavbar.tsx
@@ -1,9 +1,27 @@
 import { Box, Button, Container, Stack } from "@mui/material";
 import { NavLink } from "react-router-dom";
+import { useEffect, useState } from "react";
 import Basket  from './Basket';
 
 export default function HomeNavbar() {
-    const authMember = true; 
+     const authMember = null;
+    const [count, setCount] = useState<number>(0);
+    const [value, setValue] = useState<boolean>(true);
+
+    useEffect(()=>{
+      console.log("componentDidMount");
+        setCount(c => c + 1);
+         return() =>{
+            console.log("componentWillUnmount")
+
+        };
+
+    }, [value])
+    // Handlers
+    const buttonHandler = () => {
+        setValue(!value)
+    }
+
     return <div className="home-navbar">
        <Container className="navbar-container">
             <Stack className="menu">
@@ -89,9 +107,10 @@ export default function HomeNavbar() {
                 <Box className = {"head-main-txt"}>World's Most delicious Cousine</Box>
                 <Box className = "wel-txt">The Choice, not just a choice</Box>
                 <Box className = "service-txt">24 hours service</Box>
+                <Box className = "service-txt">{count} hours service</Box>
                 <Box className = "signup">
                     {!authMember ? (
-                        <Button variant={"contained"} className="signup-button"> SIGN UP</Button>
+                        <Button variant={"contained"} className="signup-button" onClick={buttonHandler}> SIGN UP</Button>
                     ) : null} 
                     
                 </Box>
