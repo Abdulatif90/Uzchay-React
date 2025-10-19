b2122e4b89be1e7a0db20c7efd9a6828d1490432
unknown <abdulatifsh90@gmail.com>
2025-10-19 17:24:54 +0900

    configurate socket io client

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,7 @@ import reportWebVitals from './reportWebVitals';
 import CssBaseline  from "@mui/material/CssBaseline";
 import "./css/index.css";
 import { ThemeProvider } from "@mui/material/styles"
+import {SocketProvider } from "./app/context/SocketContext"
 import './css/index.css';
 import theme from './app/MaterialTheme';
 import { BrowserRouter as Router } from 'react-router-dom';
@@ -18,12 +19,14 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
      <ContextProvider>
+        <SocketProvider>
           <ThemeProvider theme={theme}>
           <CssBaseline />
           <Router>
           <App/>
           </Router>
           </ThemeProvider>
+          </SocketProvider>
       </ContextProvider>
     </Provider>
   </React.StrictMode>,
